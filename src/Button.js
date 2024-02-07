dbfa02fc40d8f92abb77bb474cd3439d1c3102f9
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-02-07 09:10:09 +0100

    repairing with tailwind-merge library

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,4 @@
+import { twMerge } from 'tailwind-merge';
 import className from "classnames";
 // import classnames from "classnames";  // as in documentation, both imports are correct
 
@@ -11,20 +12,20 @@ function Button({
     rounded,
     outline
 }) {
-    const classes = className("px-3 py-1.5 border", {
+    const classes = twMerge(className("px-3 py-1.5 border", {
         "border-sky-600 bg-sky-500 text-white": primary,
         "border-gray-600 bg-gray-500 text-white": secondary,
         "border-green-600 bg-green-500 text-white": success,
         "border-yellow-600 bg-yellow-500 text-white": warning,
         "border-red-600 bg-red-500 text-white": danger,
         "rounded-full": rounded,
-        "bg-white": outline,  // the later rule apply final change
+        "bg-white": outline,  // the later rule applies final change
         "text-sky-500": outline && primary,
         "text-gray-500": outline && secondary,
         "text-green-500": outline && success,
         "text-yellow-500": outline && warning,
         "text-red-500": outline && danger,
-    })
+    }))
     return (
         <button className={classes}>{children}</button>
     );
