db7bfb6c73504152963210d355cb6b5e38be2a2c
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-03-06 14:17:20 +0100

    introducing Dropdown component, Accordion moved to pages/

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown"
 
 function App() {
-  const items = [
-    {
-      id: "dqhgdh",
-      label: "can I use react on a project?",
-      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
-    },
-    {
-      id: "fb34to",
-      label: "can I use java scripy on a project?",
-      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
-    },
-    {
-      id: "sfhqpqq",
-      label: "can I use python on a project?",
-      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
-    }
-  ]
-  return <Accordion items={items}/>
+  return <Dropdown />
 }
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,24 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "dqhgdh",
+      label: "can I use react on a project?",
+      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
+    },
+    {
+      id: "fb34to",
+      label: "can I use java scripy on a project?",
+      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
+    },
+    {
+      id: "sfhqpqq",
+      label: "can I use python on a project?",
+      content: "you can use, on any project you want. you can use, on any project you want. you can use, on any project you want."
+    }
+  ]
+  return <Accordion items={items}/>
+}
+
+export default AccordionPage;
