6db3ec7d6e4a49a04ceee899d83601d879052bd6
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-03-06 10:38:50 +0100

    event handler goes hybrid, + option with passing the event

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,6 +3,11 @@ import { useState } from "react";
 function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    // define outsite the mapping function (easier to read it, common way on big project) 
+    const handleClick = (x) => {
+        setExpandedIndex(x)  // 'index' in not defined! -> go hybrid shor&long -hand version of event handler 
+    }
+
     const renderedItems = items.map((item, index) => {
         const isExpended = index === expandedIndex
         console.log(isExpended)
@@ -17,7 +22,9 @@ function Accordion({ items }) {
         // key is required in list items rendered (App.js)
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+                {/* <div onClick={() => setExpandedIndex(index)}>{item.label}</div> */}
+                <div onClick={() => handleClick(index)}>{item.label}</div>
+                {/* PASSING EVENT WAY: <div onClick={handleClick}>{item.label}</div> --> handleClick = (event) => {...} */}
                 {isExpended && <div>{item.content}</div>}  
             </div>
         )
