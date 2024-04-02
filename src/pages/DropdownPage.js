780ee300ac574470a54363abe7ba5e89d4f83c65
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-04-02 13:20:38 +0200

    creating DropdownPages.js

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown"
+
+function DropdownPage() {
+  const options = [
+    {label: "Red", value: "red"},
+    {label: "Blue", value: "blue"},
+    {label: "Green", value: "green"},
+]
+  const [selection, updateSelection] = useState(null)
+  // selection: option | null
+
+  const handleSelect = (option) => {
+    console.log("option selected", option)
+    updateSelection(option)
+  }
+  
+  return <div className="flex">
+    {/* <Dropdown options={options} value={selection} onChange={handleSelect} /> */}
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+  </div>
+}
+
+export default DropdownPage;
