5356eca0cc55896215442984f036df3a9432b911
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-05-14 16:34:18 +0200

    no scroling possible when modal is shown + absolute -> fixed

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,15 @@
 import reactDOM from 'react-dom';
-import Button from './Button';
+import { useEffect } from 'react';
 
-function Modal({ onCloseClick }) {
+function Modal({ onCloseClick, children, actionBar }) {
+
+    useEffect(() => {
+        // this prevents the background from scrolling when modal is shown
+        document.body.classList.add("overflow-hidden")  // tailwind.css clas that applys overflow: hidden to the element
+        return () => {
+            document.body.classList.remove("overflow-hidden")}  // enables back when its's hidden
+    }, []);
+
     // return (
     //     <div>
     //         <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
@@ -14,10 +22,17 @@ function Modal({ onCloseClick }) {
     return reactDOM.createPortal( // this method tells react to render the modal in a different place
         // JSX to be redered
         <div>   
-            <div onClick={onCloseClick} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white">
-                I'm a modal
-                <Button danger onClick={onCloseClick}>Close Modal</Button>
+            {/* <div onClick={onCloseClick} className="absolute inset-0 bg-gray-300 opacity-80"></div> */}
+            {/* <div className="absolute inset-40 p-10 bg-white"> */}
+            {/* fixed is better than absoluet in case of button and {modal}'d be down the ModalPage (under paragraphs) */}
+            <div onClick={onCloseClick} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+            <div className="fixed inset-40 p-10 bg-white">
+                <div className="flex flex-col justify-between h-full">
+                    {children}
+                </div>
+                <div className="flex justify-end">
+                    {actionBar}
+                </div>
             </div>
         </div>,
         // location where render, inside index.html / (html?)DOM
