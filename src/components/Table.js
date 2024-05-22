2b6ae13f09dbffff630add0ccb84df43a2998abf
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-05-22 14:17:54 +0200

    implementing reusable table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,30 @@
-function Table({data}) {
-const renderedRows = data.map((row) => {
+function Table({data, config, keyFn}) {
+    const renderedHeaders = config.map((header) => {
         return (
-            <tr key={row.name}>
-                <td className="p-3">{row.name}</td>
-                <td className="p-3">
-                    <div className={`p-3 m-2 ${row.color}`}></div>
-                </td>
-                <td className="p-3">{row.score}</td>
+            <th key={header.label} className="p-3">{header.label}</th>
+        )
+    })
+
+    const renderedRows = data.map((row) => {
+        const theRow = config.map((cellConfig) => {
+            return (
+                <td key={cellConfig.label} className="p-3"> {cellConfig.render(row)} </td>
+            )
+        })
+        return (
+            <tr key={keyFn}>
+                {/* <td className="p-3"> {config[0].render(row)} </td>
+                <td className="p-3"> {config[1].render(row)} </td>
+                <td className="p-3"> {config[2].render(row)} </td> */}
+                {theRow}
             </tr>
         )
     })
 
     return (
-        // <div>
-        //     {data.length}
-        // </div>
         <table className="table-auto border-spacing-2">
             <thead>
-                <tr className="border-b-2">
-                    <th> Fruit </th>
-                    <th> Color </th>
-                    <th> Score </th>
-                </tr>
+                <tr className="border-b-2"> {renderedHeaders} </tr>
             </thead>
             <tbody> {renderedRows} </tbody>
         </table>
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -10,9 +10,35 @@ function TablePage() {
         {name: "lime", color: "bg-green-500", score: 25},
     ]
 
+    const config = [
+        {
+            label: "Fruit Name",
+            render: (fruit) => fruit.name,
+        },
+        {
+            label: "Color",
+            render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+        },
+        {
+            label: "Score",
+            render: (fruit) => fruit.score,
+            sort: () => {},
+        },
+        {
+            label: "Length of the name",
+            render: (fruit) => fruit.name.length,
+        },
+        {
+            label: "Score squered",
+            render: (fruit) => fruit.score ** 2,
+        },
+    ]
+
+    const keyFn = (fruit) => fruit.name;
+
     return(
         <div>
-            <Table data={data}/>
+            <Table data={data} config={config} keyFn={keyFn}/>
         </div>
     )
 }
