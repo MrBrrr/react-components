54b83a96076be67ac124729ca3e69d7dd347555b
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2024-05-29 14:50:56 +0200

    updating the CounetrPage.js - adding 'add a lot' option

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,51 @@
+import { useState } from "react";
 import Button from "../components/Button";
-import { useCounter } from "../hooks/use-counter";
+import Panel from "../components/Panel";
+// import { useCounter } from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
-    const { count, increment } = useCounter(initialCount);
+    const [count, setCount] = useState(initialCount);
+    const [valueToAdd, setValueToAdd] = useState(0);
+    // const { count, increment } = useCounter(initialCount);
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    const handleChange = (event) => {
+        const value = parseInt(event.target.value) || 0;
+        // this is a string so have to be transformated into number -> parseInt
+        // "" would resolve in NaN -> || 0
+        console.log(value);
+        setValueToAdd(value);
+    }
+
+    const handleSubmit = (event) => {
+        // prevent default autosubmitting of a form by the browser 
+        //  while rerendering - updating the valueToAdd by typing into input
+        event.preventDefault();
+        setCount(count + valueToAdd);
+        setValueToAdd(0);
+    }
+
     return (
-        <div>
-            <h1>Count is {count}</h1>
-            <Button onClick={increment}>Increment</Button>
-        </div>
+        <Panel className="m-3">
+            <h1 className="text-lg">Count is {count}</h1>
+            <div className="flex flex-row">
+                <Button onClick={increment}>Increment</Button>
+                <Button onClick={decrement}>Decrement</Button>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <label>Add a lot!</label>
+                <input 
+                    value={valueToAdd || ""} 
+                    // if valueToAdd is 0, it will be displayed as empty string
+                    type="number"
+                    onChange={handleChange} 
+                    className="p-1 m-3 bg-gray-50 border border-gray-300"
+                />
+                <Button>Add it!</Button>
+            </form>
+        </Panel>
     );
 }
 
